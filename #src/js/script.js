69a8bb988fb720d7b041f63eebd41cda62f2fdb9
dima <dmatvi4@gmail.com>
2021-12-02 16:44:50 +0100

    first commit+img

---
+++ #src/js/script.js
@@ -0,0 +1,19 @@
+//добавлення класу для webp, перевіряє чи браузер може відобразити формат
+//якщо може, то добаляє класи(webp,no-webp)
+function testWebP(callback) {
+
+var webP = new Image();
+webP.onload = webP.onerror = function () {
+callback(webP.height == 2);
+};
+webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+testWebP(function (support) {
+
+if (support == true) {
+document.querySelector('body').classList.add('webp');
+}else{
+document.querySelector('body').classList.add('no-webp');
+}
+});
+// ------------------
