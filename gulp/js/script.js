b79f6bc68e7ce37050796af7af721d1d37400c1d
dima <dmatvi4@gmail.com>
2021-12-13 02:24:31 +0100

    catalog-item

--- gulp/js/script.js
+++ gulp/js/script.js
@@ -2,24 +2,28 @@
 //якщо може, то добаляє класи(webp,no-webp)
 function testWebP(callback) {
 
-var webP = new Image();
-webP.onload = webP.onerror = function () {
-callback(webP.height == 2);
-};
-webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+   var webP = new Image();
+   webP.onload = webP.onerror = function () {
+      callback(webP.height == 2);
+   };
+   webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
 }
 testWebP(function (support) {
 
-if (support == true) {
-document.querySelector('body').classList.add('webp');
-}else{
-document.querySelector('body').classList.add('no-webp');
-}
+   if (support == true) {
+      document.querySelector('body').classList.add('webp');
+   } else {
+      document.querySelector('body').classList.add('no-webp');
+   }
 });
 // ------------------
 var swiper = new Swiper(".slider__block", {
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      });
+   navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+   },
+   autoplay: {
+      delay: 3000,
+   },
+   
+});
