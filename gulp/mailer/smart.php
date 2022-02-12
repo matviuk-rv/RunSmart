c9a81f4a255ff741151e35b761259227586fcb6e
matviuk-rv <dmatvi4@gmail.com>
2022-02-13 00:05:41 +0100

    fix:mailer2

--- gulp/mailer/smart.php
+++ gulp/mailer/smart.php
@@ -13,12 +13,12 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'test.matviuk@gmail.com';                 // Наш логин
-$mail->Password = 'test123a';                           // Наш пароль от ящика
+$mail->Username = '';                 // Наш логин
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('test.matviuk@gmail.com', 'RunSmart(test)');   // От кого письмо 
+$mail->setFrom('', 'RunSmart(test)');   // От кого письмо 
 $mail->addAddress('');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
